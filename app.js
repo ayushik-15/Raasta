6a61f25a77dce5c631e470320924977292a1fd09
ayushik-15 <ayushikushwaha5144@gmail.com>
2026-09-21 21:39:27 +0530

    Added root redirect

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 if(process.env.NODE_ENV != "production"){
     require("dotenv").config();
 }
+app.get('/', (req, res) => {
+  res.redirect('/listings'); 
+});
 
 const express= require("express");
 const app=express();
